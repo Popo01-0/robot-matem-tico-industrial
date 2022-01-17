f5356ca7a9355f512dfcda0c66f4a6a74663f8d1
Popo01-0 <79619756+Popo01-0@users.noreply.github.com>
2022-01-17 15:07:35 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function si() {
+    document.getElementById('si').style.display = 'block';
+    document.getElementById('no').style.display = 'none';
+}
+
+function no() {
+    document.getElementById('si').style.display = 'none';
+    document.getElementById('no').style.display = 'block';
+}
